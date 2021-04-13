3b48ae53965549c4625c7e0ccf6bc5288183cf8c
karam-sudo <karamalhalabi1994@outlook.com>
2021-04-13 11:12:55 +0300

    RelationShip created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Reviews;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function reviews(){
+
+        return $this->hasMany(Reviews::class);
+
+    }
 }
